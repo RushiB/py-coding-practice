9e1ebd36abef7f162ef8dc672ed82d3301267059
Rushi <rushidevbuddha@live.com>
2024-03-16 15:01:10 -0700

    added alternative approach in merge-sort

--- libraries/sorting/merge_sort.py
+++ libraries/sorting/merge_sort.py
@@ -1,6 +1,6 @@
 
 def merge_sort(list1, start_idx, end_idx):
-    dbg_print = False
+    dbg_print = 0
     if dbg_print: print(f'entered merge_sort({start_idx}:{end_idx})')
     
     if start_idx < end_idx:
@@ -9,23 +9,37 @@ def merge_sort(list1, start_idx, end_idx):
         if end_idx - start_idx > 1:
             merge_sort(list1,start_idx, mid-1)
             merge_sort(list1,mid, end_idx)
+            # OR method-2
+            # merge_sort(list1,start_idx, mid)
+            # merge_sort(list1,mid+1, end_idx)
         merge(list1, start_idx, mid, end_idx)
     else:
         return
 
 def merge(list1, left_idx, mid_idx, right_idx):
-    dbg_print = False
+    dbg_print = 0
     if dbg_print: print(f'entered merge({left_idx}:{mid_idx}:{right_idx})')
     left_arr, right_arr = [], []
     i = left_idx
-    while ((i < mid_idx) or (len(left_arr) < 1)):
+    
+    while (i < mid_idx) or (len(left_arr) < 1):
         left_arr.append(list1[i])
         i = i+1
-    
     j = i
     while j <= right_idx:
         right_arr.append(list1[j])
         j = j+1
+
+    # OR method-2
+    # while ((i <= mid_idx)):
+    #     left_arr.append(list1[i])
+    #     i = i+1
+    # j = i
+    # while j <= right_idx :
+    #     right_arr.append(list1[j])
+    #     j = j+1
+
+
     left_ref, right_ref = 0,0
     main_ref = left_idx
     while left_ref < len(left_arr) and right_ref < len(right_arr):
@@ -66,4 +80,17 @@ def test_merge_sort():
 
 def sort_and_print(list1):
     merge_sort(list1, 0, len(list1)-1)
-    print(f'sorted list is: {list1}')
+    print(f'sorted list is: {list1}', end=' ')
+    test_sorted_array(list1)
+
+def test_sorted_array(list1): # ascending order
+    passed = 1
+    for i in range(1, len(list1)):
+        if list1[i] > list1[i-1]:
+            continue
+        else:
+            passed = 0
+            break
+    if passed: print('PASSED!')
+    else: print('FAILED!')
+
